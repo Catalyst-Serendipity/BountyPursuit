c81cc0dd2450dede69aa7b148cc83cc15e29af37
Vega Nicholas S. <veganicholas123@gmail.com>
2025-02-14 14:58:43 +0700

    cs: change header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -50,29 +50,13 @@ Copyright (c) 2024 - present nicholass003
 | | | | | (__| | | | (_) | | (_| \__ \__ \ |_| | |_| |__) |
 |_| |_|_|\___|_| |_|\___/|_|\__,_|___/___/\___/ \___/____/
 
-The use of this software is granted only to individuals or organizations who have obtained
-a valid license from the copyright owner. The license is non-transferable and is limited to
-personal, non-commercial use.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-Any form of distribution, reproduction, or use for commercial purposes, whether directly or
-indirectly, is strictly prohibited without the express written consent of the copyright owner.
-
-Modification, decompilation, or reverse engineering of the software is not permitted.
-
-By using the software, you agree to abide by the terms of this license.
-
-The software is provided "as is," without warranty of any kind, express or implied,
-including but not limited to the warranties of merchantability, fitness for a particular
-purpose, and noninfringement. In no event shall the authors or copyright holders be
-liable for any claim, damages, or other liability, whether in an action of contract,
-tort, or otherwise, arising from, out of, or in connection with the software or the use
-or other dealings in the software.
-
-For inquiries regarding licensing options, please contact the copyright owner.
-
-@author nicholass033
-
-Developed by: Catalyst Serendipity
+@author  nicholass003
+@link    https://github.com/nicholass003/
 
 
 BODY,
@@ -92,9 +76,11 @@ BODY,
 			'anonymous_class' => false,
 		],
 		'no_closing_tag' => true,
-		'no_empty_phpdoc' => false,
+		'no_empty_phpdoc' => true,
 		'no_extra_blank_lines' => true,
-		'no_superfluous_phpdoc_tags' => false,
+		'no_superfluous_phpdoc_tags' => [
+			'allow_mixed' => true,
+		],
 		'no_trailing_whitespace' => true,
 		'no_trailing_whitespace_in_comment' => true,
 		'no_whitespace_in_blank_line' => true,
@@ -118,8 +104,8 @@ BODY,
 			'method' => null,
 			'const' => null
 		],
-		'phpdoc_trim' => false,
-		'phpdoc_trim_consecutive_blank_line_separation' => false,
+		'phpdoc_trim' => true,
+		'phpdoc_trim_consecutive_blank_line_separation' => true,
 		'return_type_declaration' => [
 			'space_before' => 'one'
 		],
